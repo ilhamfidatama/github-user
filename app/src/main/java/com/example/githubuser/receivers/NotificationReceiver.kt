da962529ff76797fec81eaaa160077b490a65f86
ilhamfidatama <ilhamfidatama@gmail.com>
2020-05-06 17:07:49 +0800

    aksi di bagian notifikasi langsung menuju aplikasi + intent ke MainActivity

--- app/src/main/java/com/example/githubuser/receivers/NotificationReceiver.kt
+++ app/src/main/java/com/example/githubuser/receivers/NotificationReceiver.kt
@@ -2,11 +2,13 @@ package com.example.githubuser.receivers
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
+import com.example.githubuser.MainActivity
 import com.example.githubuser.R
 import com.example.githubuser.helper.Utils
 
@@ -28,7 +30,10 @@ class NotificationReceiver: BroadcastReceiver() {
 
     private fun createDailyNotification(context: Context){
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val intent = Intent(context, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
         val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setContentIntent(pendingIntent)
             .setContentTitle(context.resources.getString(R.string.app_name))
             .setContentText(context.resources.getString(R.string.daily_notification_text))
             .setSmallIcon(R.drawable.ic_update_icon_24dp)
